f4dfa3ccff4e4507da028808fdca4cc9fcd08241
michael <michaelrio9966@gmail.com>
2025-01-24 17:47:50 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,52 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Indonesia Emergency Responder</title>
-    <!-- Link to AdminLTE CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1.0/dist/css/adminlte.min.css">
-    <!-- Link to Bootstrap CSS (required for AdminLTE) -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css">
-    <style>
-        body, html {
-            height: 100%;
-        }
-        .centered {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100%;
-        }
-        .card {
-            width: 100%;
-            max-width: 500px;
-        }
-    </style>
+    <title>Simple Page</title>
+    <!-- Link to Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+<body class="d-flex justify-content-center align-items-center" style="height: 100vh;">
 
-<body class="hold-transition">
-    <div class="container-fluid d-flex centered">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Silahkan Pilih</h3>
-            </div>
-            <div class="card-body text-center">
-                <!-- Buttons List -->
-                <!-- Login Button -->
-                <a href="https://id-responder.org/" class="btn btn-primary" style="margin-right:10px">Login</a>
-                    <!-- Register Button -->
-                <a href="https://id-responder.org/form_reg" class="btn btn-success">Daftar Anggota</a>
-            </div>
-        </div>
+    <div class="text-center">
+        <a href="https://id-responder.org/" class="btn btn-primary btn-lg m-2" role="button">Laporan</a>
+        <a href="https://id-responder.org/form_reg" class="btn btn-secondary btn-lg m-2" role="button">Pendaftaran Anggota</a>
     </div>
 
-    <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1.0/dist/js/adminlte.min.js"></script>
-
+    <!-- Link to Bootstrap JS (Optional) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
 </html>
