bd2eba1bb95a03eb3cc1baf7e0fd657b39429c66
michael <michaelrio9966@gmail.com>
2025-01-24 17:50:27 +0700

    update

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -2,9 +2,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('home') }}" class="brand-link">
-        <!-- <img src="{{asset('img/frd-logo.jpg')}}" alt="Indonesia Emergency Responder" class="brand-image img-circle elevation-3"
-            style="opacity: .8"> -->
-        <span class="brand-text font-weight-light">Indonesia Emergency Responder</span>
+        <img src="{{asset('img/frd-logo.jpg')}}" alt="Indonesia Emergency Responder" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
+        <span class=" font-weight-light">Indonesia Emergency Responder</span>
     </a>
 
     <!-- Sidebar -->
