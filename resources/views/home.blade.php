aa7474030a16a602d188f5a4e29324d4804702ff
michael <michaelrio9966@gmail.com>
2025-01-24 17:41:01 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,30 +9,39 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1.0/dist/css/adminlte.min.css">
     <!-- Link to Bootstrap CSS (required for AdminLTE) -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css">
+    <style>
+        body, html {
+            height: 100%;
+        }
+        .centered {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+        }
+        .card {
+            width: 100%;
+            max-width: 500px;
+        }
+    </style>
 </head>
 
 <body class="hold-transition">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <!-- Card to display buttons -->
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Choose an Action</h3>
-                    </div>
-                    <div class="card-body">
-                        <!-- Buttons List -->
-                        <div class="btn-group" role="group">
-                            <!-- Login Button -->
-                            <a href="https://id-responder.org/" class="btn btn-primary">Login</a>
-                            <!-- Register Button -->
-                            <a href="https://id-responder.org/form_reg" class="btn btn-success">Daftar Anggota</a>
-                        </div>
-                    </div>
+    <div class="container-fluid d-flex centered">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Choose an Action</h3>
+            </div>
+            <div class="card-body text-center">
+                <!-- Buttons List -->
+                <div class="btn-group" role="group">
+                    <!-- Login Button -->
+                    <a href="https://id-responder.org/index" class="btn btn-primary">Login</a>
+                    <!-- Register Button -->
+                    <a href="https://id-responder.org/form_reg" class="btn btn-success">Daftar Anggota</a>
                 </div>
             </div>
         </div>
-    </div>
     </div>
 
     <!-- Scripts -->
