699b681c2be8bca62b3704327a3fe17ab677ea71
michael <michaelrio9966@gmail.com>
2025-02-04 01:04:44 +0700

    update daftar2

--- resources/views/wilayah/index.blade.php
+++ resources/views/wilayah/index.blade.php
@@ -28,7 +28,7 @@
                         <td>{{ $w->nama_wilayah }}</td>
                         <td>{{ $w->supervisor }}</td>
                         <td>
-                        <select class="form-control" data-id="{{ $w->id }}" onchange="updateStatus(this)" id="status-{{ $w->id }}">
+                        <select class="form-control" data-id="{{ $w->id }}" onchange="updateStatus(this)" id="status-{{ $w->id_wilayah }}">
     <option value="menunggu persetujuan" @if($w->status == 'menunggu persetujuan') selected @endif>Menunggu Persetujuan</option>
     <option value="disetujui" @if($w->status == 'disetujui') selected @endif>Disetujui</option>
     <option value="ditolak" @if($w->status == 'ditolak') selected @endif>Ditolak</option>
