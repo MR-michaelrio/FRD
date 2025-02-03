776e0d5d484678bdc031def57704dfe0f0a7c3d9
michael <michaelrio9966@gmail.com>
2025-02-03 21:45:05 +0700

    update daftar2

--- resources/views/daftar2.blade.php
+++ resources/views/daftar2.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body class="bg-light d-flex justify-content-center align-items-center">
     <div class="container p-4 bg-white rounded shadow-lg" style="max-width: 500px;">
-            <img src="logo.png" alt="Logo Lembaga" class="mb-2" style="max-width: 80px;">
+            <img src="{{asset('img/frd-logo.jpg')}}" alt="Logo Lembaga" class="mb-2" style="max-width: 80px;">
             <h2 class="h4">Form Registrasi Pemegang Radio</h2>
         <form>
             <div class="mb-2">
