e9535b36a89c05523fe5ee93131789785be8b371
michael <michaelrio9966@gmail.com>
2025-02-03 21:44:36 +0700

    update daftar2

--- resources/views/daftar2.blade.php
+++ resources/views/daftar2.blade.php
@@ -6,7 +6,7 @@
     <title>Form Registrasi Pemegang Radio</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body class="bg-light d-flex justify-content-center align-items-center vh-100">
+<body class="bg-light d-flex justify-content-center align-items-center">
     <div class="container p-4 bg-white rounded shadow-lg" style="max-width: 500px;">
             <img src="logo.png" alt="Logo Lembaga" class="mb-2" style="max-width: 80px;">
             <h2 class="h4">Form Registrasi Pemegang Radio</h2>
