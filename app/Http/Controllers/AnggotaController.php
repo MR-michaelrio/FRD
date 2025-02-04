506830a3db0c17b7e282ead5a71f97a0f9b65c4b
michael <michaelrio9966@gmail.com>
2025-02-04 17:46:32 +0700

    update daftar2

--- app/Http/Controllers/AnggotaController.php
+++ app/Http/Controllers/AnggotaController.php
@@ -151,6 +151,6 @@ class AnggotaController extends Controller
             'id_anggota' => $a->id_anggota
         ]);
 
-        return redirect()->route('anggota.daftar')->with('success', 'Data Berhasil Ditambahkan');
+        return redirect()->route('anggota.indexdaftar')->with('success', 'Data Berhasil Ditambahkan');
     }
 }
