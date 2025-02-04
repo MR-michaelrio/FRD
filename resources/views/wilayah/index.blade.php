1e68cf3d53ab056b8f31f044a11b2ad97ce7b122
michael <michaelrio9966@gmail.com>
2025-02-04 16:19:24 +0700

    update daftar2

--- resources/views/wilayah/index.blade.php
+++ resources/views/wilayah/index.blade.php
@@ -95,6 +95,7 @@
                     <option data-select2-id="37">Texas</option>
                     <option data-select2-id="38">Washington</option>
                   </select>
+                  <span class="select2 select2-container select2-container--default select2-container--below select2-container--open select2-container--focus" dir="ltr" data-select2-id="2" style="width: 100%;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="true" tabindex="0" aria-disabled="false" aria-labelledby="select2-ntdd-container" aria-owns="select2-ntdd-results" aria-activedescendant="select2-ntdd-result-5tby-Alabama"><span class="select2-selection__rendered" id="select2-ntdd-container" role="textbox" aria-readonly="true" title="Alabama">Alabama</span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
 
                 <!-- <select class="form-control select2" style="width: 100%;" name="supervisor" id="supervisor" data-select2-id="1" tabindex="-1" aria-hidden="true">
                     <option value="test">test</option>
