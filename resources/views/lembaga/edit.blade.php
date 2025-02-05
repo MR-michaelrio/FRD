3a34245021cb9f438bb9866c4dfe7ae54d71b8af
michael <michaelrio9966@gmail.com>
2025-02-05 22:41:38 +0700

    update

--- resources/views/lembaga/edit.blade.php
+++ resources/views/lembaga/edit.blade.php
@@ -1,44 +1,37 @@
 @extends('template.master')
 
-@section('title', 'Edit Lembaga')
+@section('title')
+Lembaga
+@endsection
 
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-md-8">
-        <div class="card card-primary">
-            <div class="card-header">
-                <h3 class="card-title">Edit Lembaga</h3>
+<div class="col-12">
+    <div class="card card-primary">
+        <!-- /.card-header -->
+        <!-- form start -->
+        <form action="{{ route('lembaga.update', $lembaga->id_lembaga) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @METHOD('PUT')
+            <div class="card-body">
+                <div class="form-group">
+                    <label>Nama Lembaga</label>
+                    <input type="text" class="form-control" name="nama_lembaga" value="{{ $lembaga->nama_lembaga }}" required>
+                </div>
+                <div class="form-group">
+                    <label>Logo</label>
+                    <input type="file" class="form-control" name="logo_lembaga" accept="image/*">
+                    @if($lembaga->logo_lembaga)
+                        <p class="mt-2">Logo Saat Ini:</p>
+                        <img src="{{ asset($lembaga->logo_lembaga) }}" width="100">
+                    @endif                
+                </div>
             </div>
-            <form action="{{ route('lembaga.update', $lembaga->id_lembaga) }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                @method('PUT')
-                <div class="card-body">
-                    <div class="form-group">
-                        <label for="nama_lembaga">Nama Lembaga</label>
-                        <input type="text" class="form-control" id="nama_lembaga" name="nama_lembaga" value="{{ $lembaga->nama_lembaga }}" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="logo_lembaga">Logo Lembaga</label>
-                        <div class="input-group">
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="logo_lembaga" name="logo_lembaga" accept="image/*">
-                                <label class="custom-file-label" for="logo_lembaga">Pilih Logo</label>
-                            </div>
-                        </div>
-                        @if($lembaga->logo_lembaga)
-                            <div class="mt-3">
-                                <p>Logo Saat Ini:</p>
-                                <img src="{{ asset($lembaga->logo_lembaga) }}" class="img-thumbnail" width="120">
-                            </div>
-                        @endif
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <button type="submit" class="btn btn-primary">Update</button>
-                    <a href="{{ route('lembaga.index') }}" class="btn btn-default">Kembali</a>
-                </div>
-            </form>
-        </div>
+            <!-- /.card-body -->
+
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
     </div>
 </div>
 @endsection
