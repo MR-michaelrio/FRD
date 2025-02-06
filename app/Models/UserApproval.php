64e2fdaf891892f9bd39d8f1921bc5ecfb55d0b2
michael <michaelrio9966@gmail.com>
2025-02-07 01:18:33 +0700

    update

--- app/Models/UserApproval.php
+++ app/Models/UserApproval.php
@@ -8,7 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class UserApproval extends Model {
     use HasFactory;
     protected $table = "user_approvals";
+    public $timestamps = false;
     protected $fillable = ['id_user', 'supervisor', 'approved_at'];
+    
     public function user() {
         return $this->belongsTo(User::class, 'id_user');
     }
