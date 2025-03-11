045702fa8e3f7f0be6f69d55ac83ce680879bc76
michael <michaelrio9966@gmail.com>
2025-03-12 00:24:12 +0700

    update

--- resources/views/anggota/approval.blade.php
+++ resources/views/anggota/approval.blade.php
@@ -30,12 +30,12 @@ Anggota
                         <td>{{ $agt->email }}</td>
                         <td>{{ $agt->isApproved() ? 'active' : 'pending' }}</td>
                         <td>
-                        @if($agt->user && !$agt->user->isApproved() && !$agt->user->approvals()->where('supervisor', auth()->id())->exists())
-        <form method="POST" action="{{ route('approveUser', $agt->id) }}">
-            @csrf
-            <button type="submit">Approve</button>
-        </form>
-    @endif
+                            @if(!$agt->isApproved() && !$isApprovedByMe)
+                                <form method="POST" action="{{ route('approveUser', $agt->id) }}">
+                                    @csrf
+                                    <button type="submit">Approve</button>
+                                </form>
+                            @endif
                         </td>
                     </tr>
                     @endforeach
