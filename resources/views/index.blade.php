91a63507466568cac3895776c40b6692063aa979
michael <michaelrio9966@gmail.com>
2025-03-12 15:53:56 +0700

    update

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+@extends('template.master')
+
+@section('title')
+    Wilayah
+@endsection
+
+@section('css')
+<style>
+.status-buttons .btn {
+    margin-right: 5px;
+}
+</style>
+@endsection
+
+@section('content')
+<div class="col-12">
+    <div class="card">
+        <!-- /.card-header -->
+        <img src="{{asset('Rifky.webp')}}" alt="" srcset="">
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+</div>
+
+@endsection
