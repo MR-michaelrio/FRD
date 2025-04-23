39fd83383e4a3ee47a96f7509e8cc6870c8d5f7b
michael <michaelrio9966@gmail.com>
2025-04-23 22:25:41 +0700

    update

--- app/Http/Controllers/AbsenController.php
+++ app/Http/Controllers/AbsenController.php
@@ -31,8 +31,6 @@ class AbsenController extends Controller
                     ->where('wilayah', $wilayah)
                     ->get();
 
-        return $anggota->Regu;
-
         $ag = Anggota::where('wilayah', $wilayah)->get();
         $namawilayah = Wilayah::where('id_wilayah', $wilayah)->first();
 
