e1488822114cc851b2d8807c9f2bf9e9dae0500f
michael <michaelrio9966@gmail.com>
2025-06-25 15:29:52 +0700

    update

--- resources/views/qr.blade.php
+++ resources/views/qr.blade.php
@@ -8,7 +8,7 @@
   <img id="qrImage" src="" style="width:300px;">
 
   <script>
-    const ws = new WebSocket('wss://101.255.101.60:7071');
+const socket = new WebSocket("wss://wa.id-responder.org:7071");
 
     ws.onmessage = (event) => {
       const message = JSON.parse(event.data);
