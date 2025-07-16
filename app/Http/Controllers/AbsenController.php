cd511e54e9fcdbf5f07fea42612d49bd9b6eeda9
michael <michaelrio9966@gmail.com>
2025-07-16 19:19:39 +0700

    update

--- app/Http/Controllers/AbsenController.php
+++ app/Http/Controllers/AbsenController.php
@@ -44,8 +44,7 @@ class AbsenController extends Controller
         $anggota = anggota::where('role','!=','anggota')->where('wilayah','1')->get();
         $ag = anggota::all()->where('wilayah','1');
         $id = 'Jakarta';
-        $namawilayah = Wilayah::where('id_wilayah', $id)->first();
-        return $namawilayah;
+        $namawilayah = Wilayah::where('nama_wilayah', $id)->first();
         return view('absensi.absen', compact('anggota','ag','id', 'namawilayah'));
     }
 
@@ -54,7 +53,7 @@ class AbsenController extends Controller
         $anggota = Anggota::where('role', '!=', 'anggota')->where('wilayah', '2')->get();
         $ag = anggota::all()->where('wilayah','2');
         $id = 'Bekasi';
-        $namawilayah = Wilayah::where('id_wilayah', $id)->first();
+        $namawilayah = Wilayah::where('nama_wilayah', $id)->first();
         return view('absensi.absen', compact('anggota','ag','id','namawilayah'));
     }
 
