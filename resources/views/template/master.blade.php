4813d32ade37ad17e1947f274ee34f07a4ef78c6
michael <michaelrio9966@gmail.com>
2025-12-31 02:38:50 +0700

    Update Notif9

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -87,7 +87,7 @@
                     <div class="row">
                         <div class="col-12">
                             <button id="enableNotif" class="btn btn-success">
-  Aktifkan Notifikasi
+  Aktifkan Notifsikasi
 </button>
 
                             @if(auth()->check() && auth()->user()->level === 'basic')
